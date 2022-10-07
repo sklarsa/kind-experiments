c7464e824aaa1465c2953b02d26166ee7b562538
Steve Sklar <steven@questdb.io>
2022-10-07 14:07:05 -0400

    Adds dynamic orgids in promtail

--- loki-distributed/start.sh
+++ loki-distributed/start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 kind create cluster --config kind.yaml
+kubectx kind-kind
 
 ## Install and configure loki (minio included), grafana, and promtail
 
 helm repo add grafana https://grafana.github.io/helm-charts
-helm upgrade --install loki grafana/loki -f loki.yaml
-helm upgrade --install promtail grafana/promtail -f promtail.yaml
-helm upgrade --install grafana grafana/grafana
+helm upgrade --install loki grafana/loki -f loki.yaml -n logging --create-namespace
+helm upgrade --install promtail grafana/promtail -f promtail.yaml -n logging
+helm upgrade --install grafana grafana/grafana -n logging
+
+## Install questdb in a separate namespace
+helm repo add questdb https://helm.questdb.io/
+helm upgrade --install questdb questdb/questdb -n questdb-tenant-1 --create-namespace
