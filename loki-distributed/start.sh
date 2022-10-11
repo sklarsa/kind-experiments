8231958db33c33613ef09ddebc2edae57f719bcf
Steve Sklar <steven@questdb.io>
2022-10-11 13:36:59 -0400

    Adds reverse proxy

--- loki-distributed/start.sh
+++ loki-distributed/start.sh
@@ -4,12 +4,18 @@ kind create cluster --config kind.yaml
 kubectx kind-kind
 
 ## Install and configure loki (minio included), grafana, and promtail
-
 helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
 helm upgrade --install loki grafana/loki -f loki.yaml -n logging --create-namespace
 helm upgrade --install promtail grafana/promtail -f promtail.yaml -n logging
 helm upgrade --install grafana grafana/grafana -n logging
 
+## caddy for reverse proxy
+kubectl apply -f caddy.yaml
+
 ## Install questdb in a separate namespace
 helm repo add questdb https://helm.questdb.io/
+helm repo update
+
 helm upgrade --install questdb questdb/questdb -n questdb-tenant-1 --create-namespace
